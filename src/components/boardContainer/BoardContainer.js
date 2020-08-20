2d6874948ad1322361ecfb7b132b39bff98305cc
Brooke Nemchak <bnemchak@gmail.com>
2020-08-19 20:54:28 -0400

    day two class

--- src/components/boardContainer/BoardContainer.js
+++ src/components/boardContainer/BoardContainer.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
+import Board from '../Board/Board';
+
+import boardsData from '../../helpers/data/boardData';
+import authData from '../../helpers/data/authData';
+
 class BoardContainer extends React.Component {
+  state = {
+    boards: [],
+  }
+
+  componentDidMount() {
+    boardsData.getBoardsbyUid(authData.getUid())
+      .then((boards) => this.setState({ boards }))
+      .catch((err) => console.error('get boards broke!!, err'));
+  }
+
   render() {
+    const { boards } = this.state;
+
+    const boardCard = boards.map((board) => <Board key={board.id} board={board}/>);
+
     return (
-      <h2>BoardContainer Here</h2>
+      <div className="card-columns">
+        {boardCard}
+      </div>
     );
   }
 }
